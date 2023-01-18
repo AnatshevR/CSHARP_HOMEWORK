d667ef02cea22875d09aec6d02db7c45284a01c4
Roman_Anatshev <romananatshev@gmail.com>
2023-01-18 20:58:19 +0300

    TASK4 maximum of 3

---
+++ TASK4_MAXof3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("enter first number");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter second number");
+int N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter third number");
+int N3 = int.Parse(Console.ReadLine());
+
+int max = N1;
+if(N2>max) max=N2;
+if(N3>max) max=N3;
+Console.Write("the maximum is:");
+Console.WriteLine(max);
