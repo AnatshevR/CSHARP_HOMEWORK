ee4f11112210000c864502c54f5db0e28f916369
Roman_Anatshev <romananatshev@gmail.com>
2023-01-18 12:52:33 +0300

    TASK3 DAY of WEEK

---
+++ TASK3_dayOFweek/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter number from 1 to 7");
+int x = int.Parse(Console.ReadLine());
+if (x==1)
+{
+    Console.WriteLine("this is Monday");
+}
+else if (x==2)
+{
+    Console.WriteLine("this is Tuesday");
+}
+else if (x ==3)
+{
+    Console.WriteLine("this is wednesday");
+}
+else if (x==4)
+{
+    Console.WriteLine("this is thursday");
+}
+else if (x==5)
+{
+    Console.WriteLine("this is Friday");
+}
+else if (x==6)
+{
+    Console.WriteLine("this is saturday");
+}
+else
+{
+    Console.WriteLine("this is sunday");
+}
