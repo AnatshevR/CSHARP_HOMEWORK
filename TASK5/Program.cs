e99b8900008a352ab1905d64d8520884b9edd77f
Roman_Anatshev <romananatshev@gmail.com>
2023-01-18 21:19:05 +0300

    task5 EVENcheck

---
+++ TASK5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter any integer number");
+int x = int.Parse(Console.ReadLine());
+
+if (x%2==0)
+{
+    Console.Write(x);
+    Console.WriteLine(" - is the even number");
+}
+else
+{
+    Console.Write(x);
+    Console.WriteLine(" - is the odd number");
+}
