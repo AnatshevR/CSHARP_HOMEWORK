0f9b227b3310478e0d40fa337ad764bac1971604
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 12:02:42 +0300

    TASK6 all of EVEN from 1 to N

---
+++ TASK6_ALLofEVEN/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("N= ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+int i = 1;
+while(i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write("Even numbers are: ");
+        Console.WriteLine(  i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+            
+}
