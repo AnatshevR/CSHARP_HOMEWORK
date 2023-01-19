0a3682024451a012e4923c1df93cbfb46236dfae
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 19:27:03 +0300

    task11 multiplicity Check

---
+++ TASK11_MultiplicityOfanyNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("enter first positive integer");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("enter any divider");
+int divider = int.Parse(Console.ReadLine());
+int result = number / divider;
+int remainder = number % divider;
+if(number%divider==0)
+{
+    Console.WriteLine("your number is multiple");
+    Console.Write("Result is: ");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.Write("you have remainder: ");
+    Console.WriteLine(remainder);
+}
