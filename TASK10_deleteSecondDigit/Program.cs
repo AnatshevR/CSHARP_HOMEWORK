6132e3f9bab465c2363d2d3d1532becad6ff6036
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 18:50:48 +0300

    Task10 delete second digit

---
+++ TASK10_deleteSecondDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random ().Next (100,1000);
+Console.Write("we have number:  ");
+Console.WriteLine(number);
+int x = number / 100;
+int y = number % 10;
+int result = x * 10 + y;
+Console.Write("if we delete the center digit, the result will be: ");
+Console.WriteLine(result);
