0948701529cdc930e0b323fc7e93ba3e7aefd548
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 18:31:17 +0300

    TASK9 compare two digits

---
+++ TASK9_CompareTwoDigits/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random ().Next(10,100);
+Console.Write("we have number:  ");
+Console.WriteLine(numberA);
+int x = numberA / 10;
+int y = numberA % 10;
+if (x>y)
+{
+    Console.Write("the bigger digit is:  ");
+    Console.WriteLine(x);
+}
+else
+{
+    Console.Write("the bigger digit is: ");
+    Console.WriteLine(y);
+}
