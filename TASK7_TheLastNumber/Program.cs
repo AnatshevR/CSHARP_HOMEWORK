a014bc075fc68370c1a7b255adc0b9dd81cd2d86
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 12:32:02 +0300

    Task 7 the third digit

---
+++ TASK7_TheLastNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter any three-digit number");
+int x = int.Parse(Console.ReadLine());
+if((x>99)&(x<1000))
+{
+    int y = x % 10 ;
+    Console.WriteLine ("the third digit is:  ");
+    Console.WriteLine (y);
+}
+else
+{
+    Console.WriteLine("This number isn't requered condition");
+}
+
