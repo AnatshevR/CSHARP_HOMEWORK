55d549c45b8d68cf0f55c4e7a63d08154a38a9ef
Roman_Anatshev <romananatshev@gmail.com>
2023-01-19 12:43:29 +0300

    Task 8 the second digit

---
+++ TASK8_the2ndDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter any three-digit number");
+int x = int.Parse(Console.ReadLine());
+if((x>99)&(x<1000))
+{
+    int y = x % 100 ;
+    int w = y / 10;
+    Console.WriteLine ("the second digit is:  ");
+    Console.WriteLine (w);
+}
+else
+{
+    Console.WriteLine("This number isn't requered condition");
+}
