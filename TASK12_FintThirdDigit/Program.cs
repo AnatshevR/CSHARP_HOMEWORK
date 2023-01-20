51bac391ac0a7e50b0253dfe787f5b9693db7073
Roman_Anatshev <romananatshev@gmail.com>
2023-01-20 11:30:44 +0300

    TASK 12 the third from the end

---
+++ TASK12_FintThirdDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter any integer");
+int x = int.Parse(Console.ReadLine());
+int remainder = x % 1000;
+int result = remainder /100;
+if (x>99 || x<-99)
+{
+    Console.Write("the third digit from the end is: ");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("there is no third digit");
+}
