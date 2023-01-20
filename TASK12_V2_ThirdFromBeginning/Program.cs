d582642b190208a1c66bef1c66c82afd7f423be5
Roman_Anatshev <romananatshev@gmail.com>
2023-01-20 12:47:48 +0300

    TASK12 third from beginning

---
+++ TASK12_V2_ThirdFromBeginning/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter any integer");
+int x = int.Parse(Console.ReadLine());
+while (x <-999 || x >999)
+{
+    x = x / 10;
+}
+if (x>99 || x<-99)
+{
+    int result = x % 10;
+    Console.Write("the third digit from the beginning is: ");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("there is no third digit");
+}
