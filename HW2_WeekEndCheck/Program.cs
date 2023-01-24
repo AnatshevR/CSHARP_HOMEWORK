ae1503a078a39b22799f98c4c651046680a55fc0
Roman_Anatshev <romananatshev@gmail.com>
2023-01-24 10:50:29 +0300

    HW2 1st task weekend check

---
+++ HW2_WeekEndCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("enter any number according day of a week");
+int day = int.Parse(Console.ReadLine());
+if (day>5 && day <8)
+{
+    Console.WriteLine("Congratulations: this is weekend!");
+}
+else
+{
+    if (day<1 || day >7)
+    {
+        Console.WriteLine("Please try again, the number must be from 1 to 7");
+    }
+    else
+    {
+        Console.WriteLine("Work hard! This is weekday!");
+    }
+}
