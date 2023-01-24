0171f91c912ff4a2f2d575f3a2c4d9aad85ac885
Roman_Anatshev <romananatshev@gmail.com>
2023-01-24 11:16:25 +0300

    HW2 quarter coordinates

---
+++ HW2_PossibleCoordinates/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("To see a possible coordinates please choose the quarter (it must be from 1 to 4)");
+int coord = int.Parse(Console.ReadLine());
+if (coord == 1)
+{
+    Console.WriteLine("for this quarter coordinates will be:");
+    Console.WriteLine(" x>0 y>0");
+}
+else if ( coord == 2)
+{
+    Console.WriteLine("for this quarter coordinates will be:");
+    Console.WriteLine(" x<0 y>0");
+}
+else if ( coord == 3)
+{
+    Console.WriteLine("for this quarter coordinates will be:");
+    Console.WriteLine(" x<0 y<0");
+}
+else if ( coord == 4)
+{
+    Console.WriteLine("for this quarter coordinates will be:");
+    Console.WriteLine(" x>0 y<0");
+}
+else 
+{
+    Console.WriteLine("this number is out of quarter");
+}
