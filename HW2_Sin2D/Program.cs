61fad4b6d7639a66db18db53724220ccb723e714
Roman_Anatshev <romananatshev@gmail.com>
2023-01-24 11:53:40 +0300

    Distanse in 2D

---
+++ HW2_Sin2D/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter coordinate x for first point");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter coordinate y for first point");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter coordinate x for second point");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter coordinate y for second point");
+double y2 = double.Parse(Console.ReadLine());
+double result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+Console.WriteLine("the distanse between first and second point is: ");
+Console.WriteLine(result);
