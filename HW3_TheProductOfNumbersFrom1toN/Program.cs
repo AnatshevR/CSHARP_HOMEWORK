0d6cb849a17f20231bb3f474c2589e1a3375764c
Roman_Anatshev <romananatshev@gmail.com>
2023-01-26 10:42:24 +0300

    HW3 Task 3 the product of numbers from 1 to N

---
+++ HW3_TheProductOfNumbersFrom1toN/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter any integer number to count a product");
+int N = int.Parse(Console.ReadLine());
+int sum = 1;
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        sum = sum * i;
+    }
+    Console.WriteLine("the product of all digits from 1 to " + N + " = " + sum);
+}
+else
+{
+    Console.WriteLine("your number is too small and result will be 0");
+}
