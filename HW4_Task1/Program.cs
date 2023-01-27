569a799696410aba542ab8d9a17d8ccb694fbcea
Roman_Anatshev <romananatshev@gmail.com>
2023-01-27 09:48:56 +0300

    Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран

---
+++ HW4_Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int [] myarray = new int [8];
+int i =0;
+int L = myarray.Length;
+while ( i< L)
+{
+    myarray[i] = new Random().Next(0,2);
+    i++;
+}
+Console.WriteLine(string.Join ("*", myarray ));
