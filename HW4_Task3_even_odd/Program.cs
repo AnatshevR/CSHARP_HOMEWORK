c3dc5476906d12ea55f723f054722fd1dbc6d163
Roman_Anatshev <romananatshev@gmail.com>
2023-01-27 12:04:12 +0300

    Even and Odd check

---
+++ HW4_Task3_even_odd/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+Console.WriteLine("enter the amount of elemets in array");
+int L = int.Parse(Console.ReadLine());
+int [] array = new int[L];
+for (int i=0; i<L; i++)
+{
+    array[i] = new Random ().Next(100,1000);
+}
+Console.WriteLine(string.Join("*", array ));
+int index = 0;
+int even = 0;
+int odd = 0;
+while (index < L)
+{
+    if (array[index]%2==0)
+    {
+        even=even +1;
+    }
+    else
+    {
+        odd=odd +1;
+    }
+    index++;
+}
+Console.WriteLine("the amount of even numbers are: " + even);
+Console.WriteLine("the amount of odd numbers are: " + odd);
