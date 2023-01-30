42fb20bd19e3f1c251391ddca6b5383bd4fad4bb
Roman_Anatshev <romananatshev@gmail.com>
2023-01-30 11:09:22 +0300

    Найти сумму чисел одномерного массива стоящих на нечетной позиции

---
+++ HW4_Task5_sumOfOddIndexNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int [] array5 = new int [8];
+int i =0;
+int L = array5.Length;
+while ( i< L)
+{
+    array5[i] = new Random().Next(0,10);
+    i++;
+}
+int sum =0;
+for (int ind= 0;ind<L;ind++)
+{
+    if (ind%2==1)
+    {
+        sum = sum +array5[ind];
+    }
+}
+Console.WriteLine(string.Join ("*", array5 ));
+Console.WriteLine ("the sum of all numbers in odd index = " + sum);
